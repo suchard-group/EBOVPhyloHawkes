471196f6b8a585daef03a90b16c48f7015e31dc1
andrewjholbrook <homefolder@Andrews-MacBook-Pro.local>
2020-12-02 23:33:19 -0800

    centroids added to xml

---
+++ code/addPriorMean.py
@@ -0,0 +1,30 @@
+# This file generate the xml files to run BEAST analyses
+# Xiang Ji
+import argparse, os, re, numpy
+
+def main():
+	file_data = numpy.genfromtxt("EBOVPhyloHawkes/data/Location_Data_2016-05-27.csv", usecols=(14,15), skip_header=1, dtype=str,delimiter=",")
+	x = file_data[:]
+	file_data = numpy.genfromtxt("EBOVPhyloHawkes/data/Location_Data_2016-05-27.csv", usecols=0, skip_header=1, dtype=str,delimiter=",")
+	y = file_data[:]
+	with open("EBOVPhyloHawkes/xml/Makona_1610_Hawkes_v1.xml","r") as infile:
+		with open("EBOVPhyloHawkes/xml/Makona_1610_Hawkes_v2.xml","w") as outfile:
+			lines=infile.readlines()
+			for i in range(0,len(lines)):
+				s = lines[i]
+				if(re.match('(.*)<taxon id=(.*)', s)):
+					token=False
+					for k in range(0,len(y)):
+						if(re.match('(.*)\|'+y[k]+'\|(.*)', s)):
+							outfile.write(s+'\t\t\t<attr name=\"centroid\">'+x[k,0]+' '+x[k,1]+'</attr>\n')
+							token=True
+					i += 1
+					if(token==False):
+						outfile.write(s)
+				else:
+					outfile.write(s)
+
+if __name__ == '__main__':
+	main()
+
+# python get_locations_times.py
