67ff03077cbd4645b5ba2c0f01b0951e8815784f
andrewjholbrook <homefolder@Andrews-MacBook-Pro.local>
2020-12-03 21:57:39 -0800

    taxa2 added

---
+++ code/addTaxaWithLocations.py
@@ -0,0 +1,27 @@
+# This file generate the xml files to run BEAST analyses
+# Xiang Ji
+import argparse, os, re, numpy
+
+def main():
+	file_data = numpy.genfromtxt("EBOVPhyloHawkes/data/taxaWithLocations.csv", usecols=0, skip_header=1, dtype=str,delimiter=",")
+	x = file_data[:]
+	with open("EBOVPhyloHawkes/xml/Makona_1610_Hawkes_v4.xml","r") as infile:
+		with open("EBOVPhyloHawkes/xml/Makona_1610_Hawkes_v5.xml","w") as outfile:
+			lines=infile.readlines()
+			for i in range(0,len(lines)):
+				s = lines[i]
+				if(re.match('(.*)</taxa>(.*)', s)):
+					outfile.write(s)
+					outfile.write('\n\t<taxa id="taxa2">\n')
+					i += 1
+					for k in range(0,len(x)):
+						outfile.write('\t\t<taxon idref=\"'+x[k]+'\"/>\n')
+						i += 1
+					outfile.write('\t</taxa>\n')
+				else:
+					outfile.write(s)
+
+if __name__ == '__main__':
+	main()
+
+# python get_locations_times.py
