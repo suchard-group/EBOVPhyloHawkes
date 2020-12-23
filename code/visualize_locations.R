363851a496dabf6ed7086b465be18c56a452240f
andrewjholbrook <homefolder@Andrews-MacBook-Pro.local>
2020-12-23 12:53:13 -0800

    added labels and new bb for locations visualization

--- code/visualize_locations.R
+++ code/visualize_locations.R
@@ -4,7 +4,7 @@ library(readr)
 library(ggmap)
 register_google(key = "AIzaSyDzICiKTM1TA0Ux4bcBXFiwd1_1OMbizcg")
 
-df <- read_table2("output/Makona_1610_Hawkes.log", skip = 3)
+df <- read_table2("output/Makona_1610_Hawkes_Locations_Prior.log", skip = 3)
 P  <- dim(df)[2]
 R  <- dim(df)[1]
 N  <- 1366 # number of locations
@@ -15,17 +15,24 @@ for (i in 1:R) {
 }
 
 # get map
-bb <- c(left=-15.5,
-        bottom=4,
-        right=-7.3,
-        top=12.7)
+bb <- c(left=-17.5,
+        bottom=3.5,
+        right=-5.3,
+        top=13)
 
 bg <- get_stamenmap(bbox = bb,
                            zoom = 6,
-                           maptype="toner-2010")
-gg <- ggmap(bg)
+                           maptype="toner-background")
+
+                           #maptype="toner-2010")
+gg <- ggmap(bg) 
 
 gg1 <- gg + geom_point(data=data.frame(x=unlist(Locs[[R]][,1]),
                                        y=unlist(Locs[[R]][,2])),
-                       aes(color="red",x=x,y=y),inherit.aes = FALSE) 
+                       alpha=0.5, color="red2",
+                       aes(x=x,y=y),inherit.aes = FALSE) +
+  annotate(geom = "label",fontface="bold",label="Guinea",x=-11,y=11,label.size=0.5) +
+  annotate(geom = "label",fontface="bold",label="Sierra\nLeone",x=-13.5,y=7.5,label.size=0.5)+
+  annotate(geom = "label",fontface="bold",label="Liberia",x=-8.3,y=5.8,label.size=0.5)+
+  theme_nothing()
 gg1
