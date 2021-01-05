263e13376d5e5a75755309450a6b898857250452
andrewjholbrook <homefolder@Andrews-MacBook-Pro.local>
2021-01-05 15:53:20 -0800

    brownian motion to rates

--- code/Brownian1.R
+++ code/Brownian1.R
@@ -41,8 +41,27 @@ df$Colors <- c(rep("Blue",blue*rps),
                rep("Green",green*rps))
 df$Colors <- factor(df$Colors)
 
-gg <- ggplot(df,aes(x=Time,y=Displacements,color=Colors))+
-  geom_segment(x = 0,y=0,yend=0,xend=max(Time),color="black") +
+# add rate functions
+lowest <- min(df$Displacements)-50
+expfun <- dexp(seq(from=0,to=5,length.out = 6000))-1
+Displacements <- c(expfun*abs(redPath[length(redPath)]-lowest)+redPath[length(redPath)],
+                   expfun*abs(yellowPath[length(yellowPath)]-lowest)+yellowPath[length(yellowPath)],
+                   expfun*abs(purplePath[length(purplePath)]-lowest)+purplePath[length(purplePath)],
+                   expfun*abs(greenPath[length(greenPath)]-lowest)+greenPath[length(greenPath)])
+Time <- c( (max(df$Time[df$Colors=="Red"])+1):(max(df$Time[df$Colors=="Red"])+6000),
+           (max(df$Time[df$Colors=="Yellow"])+1):(max(df$Time[df$Colors=="Yellow"])+6000),
+           (max(df$Time[df$Colors=="Purple"])+1):(max(df$Time[df$Colors=="Purple"])+6000),
+           (max(df$Time[df$Colors=="Green"])+1):(max(df$Time[df$Colors=="Green"])+6000))
+Colors <- c(rep("Red",6000),
+            rep("Yellow",6000),
+            rep("Purple",6000),
+            rep("Green",6000))
+df2 <- data.frame(Displacements,Time,Colors)
+
+df3 <- rbind(df,df2)
+
+gg <- ggplot(df3,aes(x=Time,y=Displacements,color=Colors))+
+  #geom_segment(x = 0,y=0,yend=0,xend=max(Time),color="black",size=1.5) +
   geom_line(size=1.5,alpha=1) +
   scale_color_manual(values=c("#5a8ba0","#845d36","#81a66c","#532383","#b63129","#afa750")) +
   theme_void()+
