2ae91fc0c76b23ce085b019789ac25b243242c57
andrewjholbrook <homefolder@Andrews-MacBook-Pro.local>
2021-02-16 12:22:37 -0800

    pearson bash script

---
+++ code/run_analysis_pearson.bash
@@ -0,0 +1,1 @@
+java -jar -Djava.library.path=/usr/lib/jvm -Dhph.library.path=/home/aholbrook/hawkes/build -Dhph.required.flags=20 -Dhph.resource=2 /Users/homefolder/beast-mcmc/build/dist/beast.jar  -seed 666 -save_state checkpoint.state -save_every 1000000 -overwrite /home/aholbrook/EBOVPhyloHawkes/xml/Makona_1610_Hawkes_All_Taxa4.xml
