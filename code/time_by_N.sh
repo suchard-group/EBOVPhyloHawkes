18ef82b3891ef37f395736225ee3e76769d995b7
andrewjholbrook <homefolder@Andrews-MacBook-Pro.local>
2021-08-22 11:54:28 -0700

    time by n with hessian

--- code/time_by_N.sh
+++ code/time_by_N.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 cd hawkes/build/
 for n in 10000 20000 30000 40000 50000 60000 70000 80000 90000; do
-	for t in 1 2 4 6 8 10 12 14 16 18 20; do
+	./benchmark --tbb 1 --locations $n --iterations 1 --avx
+	./benchmark --tbb 2 --locations $n --iterations 1 --avx
+	for t in {4..104..10}; do
 	./benchmark --tbb $t --locations $n --iterations 1 --avx
 	done
 done
