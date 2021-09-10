45e5550ec3b4bdac5968199ba19be66e4496c688
andrewjholbrook <aholbroo@g.ucla.edu>
2021-09-10 15:27:37 -0700

    modified scripts

--- code/time_by_N.sh
+++ code/time_by_N.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 cd hawkes/build/
-for n in 10000 20000 30000 40000 50000 60000 70000 80000 90000; do
-	./benchmark --tbb 1 --locations $n --iterations 1 --avx
-	./benchmark --tbb 2 --locations $n --iterations 1 --avx
-	for t in {4..104..10}; do
-	./benchmark --tbb $t --locations $n --iterations 1 --avx
-	done
-done
+#for n in 10000 20000 30000 40000 50000 60000 70000 80000 90000; do
+#	./benchmark --tbb 1 --locations $n --iterations 1 --avx
+#	./benchmark --tbb 2 --locations $n --iterations 1 --avx
+#	for t in {4..104..10}; do
+#	./benchmark --tbb $t --locations $n --iterations 1 --avx
+#	done
+#done
 
 for n in 10000 20000 30000 40000 50000 60000 70000 80000 90000; do
-	./benchmark --gpu 1 --locations $n --iterations 100
+	./benchmark --gpu 1 --locations $n --iterations 10
 done
