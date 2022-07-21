a10afd5b35e4804104840f1765035e410773a706
andrewjholbrook <aholbroo@g.ucla.edu>
2022-07-21 10:07:53 -0700

    ready to go

--- code/getMask2.R
+++ code/getMask2.R
@@ -1,5 +1,5 @@
 setwd("~/EBOVPhyloHawkes/")
 
 #cat(c(rep(1,1366),rep(0,21811)),sep=" ",file="data/mask2.txt")
-cat(c(rep(1,370),rep(0,4009)),sep=" ",file="data/mask2.txt")
-# 365?
+cat(c(rep(1,365),rep(0,4009)),sep=" ",file="data/mask2.txt")
+# 365 sequenced with locations/times
